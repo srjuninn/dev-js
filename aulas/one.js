fc0802b8f872964029648178ce2b73801ee5975d
ryan silva <ryanferreiradasilva2006@gmail.com>
2026-02-03 10:22:30 -0300

    feat: add exercise one

---
+++ aulas/one.js
@@ -0,0 +1,20 @@
+// ### **Atividade Prática**
+
+// 1. Crie dois arquivos:
+//     - `index.html` (para rodar no navegador).
+//     - `app.js` (para rodar no Node.js).
+// 2. Em cada um, exiba:
+//     - Seu nome.
+//     - Sua idade.
+//     - O resultado da soma entre `7 + 3`.
+// 3. Teste rodar:
+//     - `index.html` → abra no navegador.
+//     - `app.js` → rode com `node app.js` no terminal.
+
+let name = 'Ryan'
+let age = 19
+let sum = 7 + 3
+
+console.log(`Seja bem vindo ${name}, sua idade é ${age}`)
+console.log(`À soma da conta 7 + 3 = ${sum}`);
+
