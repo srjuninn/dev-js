d195dff1404107c9675111a6d46749d45bb86837
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2026-02-18 10:58:36 -0300

    feat: add alert to show in navigator

--- aulas/three.js
+++ aulas/three.js
@@ -3,8 +3,8 @@ let lastName = 'Silva'
 let age = 19 
 let number = 300
 
-console.log(`Meu nome é ${name, lastName} e minha idade é ${age}`)
-console.log(`A soma de ${age} + ${number} é ${(age+number)}`)
-console.log(`A subtração de ${age} - ${number} é ${(age-number)}`)
-console.log(`A divisão de ${age} / ${number} é ${(age/number)}`)
-console.log(`A multiplicação de ${age} * ${number} é ${(age*number)}`)
+alert(`Meu nome é ${name} ${lastName} e minha idade é ${age}`)
+alert(`A soma de ${age} + ${number} é ${(age+number)}`)
+alert(`A subtração de ${age} - ${number} é ${(age-number)}`)
+alert(`A divisão de ${age} / ${number} é ${(age/number)}`)
+alert(`A multiplicação de ${age} * ${number} é ${(age*number)}`)
