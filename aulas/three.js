e631401cbab6e4984019b36345cf0a6dfa2bc040
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2026-02-18 10:53:03 -0300

    feat: add exercise three string template

---
+++ aulas/three.js
@@ -0,0 +1,10 @@
+let name = 'Ryan'
+let lastName = 'Silva'
+let age = 19 
+let number = 300
+
+console.log(`Meu nome é ${name, lastName} e minha idade é ${age}`)
+console.log(`A soma de ${age} + ${number} é ${(age+number)}`)
+console.log(`A subtração de ${age} - ${number} é ${(age-number)}`)
+console.log(`A divisão de ${age} / ${number} é ${(age/number)}`)
+console.log(`A multiplicação de ${age} * ${number} é ${(age*number)}`)
